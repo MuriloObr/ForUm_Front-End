0046bc119b9d40c697ea200106e7c0648e74e85d
MuriloObr <muriloobregao@gmail.com>
2024-08-17 18:58:19 -0300

    bug fix

--- src/api/getFunctions.ts
+++ src/api/getFunctions.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Post, Comment, User, ApiRes } from '../types/typesAPI'
+import { Post, Comment, User } from '../types/typesAPI'
 
 export const getData = {
   allPosts,
@@ -18,57 +18,59 @@ export const API_URL = devMode
 
 async function allPosts(): Promise<Post[] | []> {
   const request = await axios.get(`${API_URL}/posts`)
-  const data: ApiRes<Post[]> = request.data
+  const data: Post[] = request.data
 
   if (request.status === 204) return []
 
-  return data[1]
+  return data
 }
 
 async function allPostsFromUser(id: number): Promise<Post[]> {
   const request = await axios.get(`${API_URL}/posts/user/${id}`)
-  const data: ApiRes<Post[]> = request.data
+  const data: Post[] = request.data
 
-  return data[1]
+  if (request.status === 204) return []
+
+  return data
 }
 
 async function allCommentsFromPost(id: number): Promise<Comment[] | []> {
   const request = await axios.get(`${API_URL}/comments/${id}`, {
     withCredentials: true,
   })
-  const data: ApiRes<Comment[]> = request.data
+  const data: Comment[] = request.data
 
   if (request.status === 204) return []
 
-  return data[1]
+  return data
 }
 
 async function postByID(
   id: number,
 ): Promise<{ post: Post; comments: Comment[] }> {
   const request = await axios.get(`${API_URL}/posts/${id}`)
-  const data: ApiRes<Post> = request.data
+  const data: Post = request.data
   const comments = await allCommentsFromPost(id)
 
-  return { post: data[1], comments }
+  return { post: data, comments }
 }
 
 async function userByID(id: number): Promise<User> {
   const request = await axios.get(`${API_URL}/user/${id}`)
-  const data: ApiRes<User> = request.data
+  const data: User = request.data
 
-  return data[1]
+  return data
 }
 
 async function profile(): Promise<{ user: User; posts: Post[] | [] }> {
   const request = await axios.get(`${API_URL}/profile`, {
     withCredentials: true,
   })
-  const data: ApiRes<User> = request.data
+  const data: User = request.data
 
-  const posts = await allPostsFromUser(data[1].id)
+  const posts = await allPostsFromUser(data.id)
 
-  if (posts === undefined) return { user: data[1], posts: [] }
+  if (posts === undefined) return { user: data, posts: [] }
 
-  return { user: data[1], posts }
+  return { user: data, posts }
 }
